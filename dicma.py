64e06ded87a8fd00a852809acbd5a3cadd6ca2ab
ArtesOscuras <xavi.breix@gmail.com>
2025-07-02 14:34:53 +0200

    Update dicma.py
    Introducing machine learning function for linux

--- dicma.py
+++ dicma.py
@@ -36,6 +36,9 @@ VERBOSE = True
 OUTPUT_FILE_BULEAN = False
 MASSIVE_MODE = False
 
+# Machine learning variables
+NEIGHBORS_AMMOUNT = 20
+
 # internal stored patterns: (I took it from rockyou.txt)
 BASIC_SUFIXS = ['1', '2', '123', '12', '3', '7', '13', '5', '4', '11', '!', '07', '23', '22', '01', '21', '8', '14', '10', '08', '6', '06', '9', '15', '16', '69', '18', '17', '24', '05', '.', '09', '88', '19', '25', '20', '03', '0', '04', '27', '89', '02', '99', '26', '101', '77', '1234', '28', '33', '00', '2007', '92', '87', '93', '2006', '*', '29', '94', '90', '2008', '91', '95', '86', '55', '30', '666', '143', '31', '96', '85', '44', '32', '007', '34', '4ever', '84', '45', '#1', '2005', '78', '98', '66', '83', '82', '97', '1994', '79', '100', '1992', '1993', '81', '4life', '4eva', '12345', '1995', '1991', '777', '1990', '420', '76', '56', '111', '1989', '2000', '1987', '321', '2009', '67', '80', '75', '2004', '1996', '42', '1988', '35', '74', '72', '1986', '1985', '001', '36', '73', '54', '2003', '456', '50', '!!', '333', '68', '@', '1984', '64', '37', '65', '40', '71', '2002', '4u', '123456', '43', '555', '911', '1997', '52', '?', '999', '1983', '1982', '47', '$', '57', '2001', '41', '@hotmail.com', '1980', '38', '4me', '1981', '63', '46', '70', '2010', '58', '48', '222', '51', '62', '1979', '121', '619', '53', '789', '59', '39', '112', '1998', '1978', '000', '888', '49', '**', '247', '234', '61', '60', '..', '213', '1977', '1212', '200', '159', '1999', '@yahoo.com', '1976', '182', '786', '1!', '.com', '<3', '1975', ')', '125', '187', '214', '2k7', '147', '...', '1974', '.1', '102', '1973', '!!!', '4e', '123456789', '411', '212', '6969', '1010', '305', '124', '012', '1972', '345', '360', '1969', '009', '316', '1970', '313', '4lyf', '711', '210', '808', '122', '1313', '987', '311', '120', '#', '1971', '444', '369', '1000', '008', '500', '2012', '323', '2011', '211', '1111', '246', '215', ',', '300', '135', '567', '117', '003', '103', '113', '1968', '1122', '225', '713', '132', '209', '510', '002', '520', '1221', '310', '223', '105', '~', '127', '100pre', '110', '1967', '818', '011', '1213', '1012', '202', '312', '128', '109', '+', '108', '126', '098', '1966', '2k6', '1965', '909', '1.', '118', '1020', '3000', '115', '678', '714', '415', '1964', '013', '1123', '107', '718', '129', ']', '831', '010', '2121', '4l', '005', '145', '314', '224', '104', '2525', '131', ';', '221', '626', '1314', '2020', '357', '4lyfe', '114', '318', '1210', '1223', '916', '1023', '1230', '106', '900', '216', '006', '116', '1011', '2323', '201', '2468', '4321', '119', '504', '3r', '1963', '1214', '217', '315', '2u', '512', '1224', '412', '0123', '1231', '1022', '1121', '1013', '1001', '258', '890', '134', '150', '220', '421', '1021', '1220', '1024', '1218', '1215', '317', '199', '521', '1216', '***', '303', '515', '004', '707', '$$', '206', '1014', '219', '1025', '1211', '1962', '218', '320', '1960', '1217', '413', '4444', '727', '1205', '1225', '`', '912', '812', '410', '0n', '813', '227', '2k', '014', '100%', '1206', '231', '1228', '505', '1029', '7777', '0000', '1017', '1015', '1107', '133', '1112', '325', '130', '513', '4LIFE', '324', '@1', '1016', '511', '1018', '423', '180', '1026', '021', '1125', '1031', '1028', "'", '1203', '1204', '1104', '023', '2k8', '612', '400', '721', '805', '1105', '2112', '1124', '1120', '4you', '525', '1207', '228', '205', '913', '2222', '2013', '#2', '1103', '1202', '1961', '1027', '617', '1106', '1019', '1127', '717', '1227', '156', '250', '4evr', '1208', '712', '600', '1219', '254', '1414', '017', '226', '408', '925', '723', '322', '1959', '137', '817', '319', '2424', '151', '809', '910', '232', '/', '203', '1229', '414', '326', '327', '616', '1515', '1226', '1030', '516', '1*', '1201', '2x', '365', '144', '1129', '123!', '623', '1209', '523', '611', '1126', '1005', '9999', '1128', '=', '1004', '!1', '1102', '710', '1002', '654', '1101', '613', '419', '1108', '422', '921', '198', '1130', '425', '123.', '923', '915', '169', '615', '015', '416', '#3', '951', '610', '141', '190', '816', '328', '1958', '811', '720', '016', '828', '426', '702', '919', '191', '1a', '522', '1007', '501', '018', '823', '189', '715', '8888', '918', '54321', '716', '256', '1003', '424', '1234567', '963', '138', '@123', '207', '427', '2345', '787', '5150', '852', '242', '914', '621', '331', '4EVER', '741', '637', '614', '.123', '562', '252', '920', '301', '1109', '1222', '153', '330', '1e', '417', '1006', '530', '800', '0506', '1957', '*1', '245', '1956', '517', '622', '155', '235', '753', '514', '329', ':)', '1113', '0101', '157', '821', '722', '810', '5678', '429', '2526', '724', '527', '024', '069', '524', '418', '0607', '208', '922', '624', '142', '168', '152', '2me', '0808', '1008', '929', '561', '2b', '700', '1717', '928', '815', '233', '518', '136', '177', '0708', '625', '0102', '725', '901', '022', '822', '917', '0909', '519', '334', '526', '248', '183', '731', '927', '531', '1117', '167', '602', '1114', '1818', '814', '5555', '529', '019', '620', '1n', '1616', '1955', '824', '618', '%', '1009', '192', '719', '503', '924', '1919', '229', '1st', '@aol.com', '926', "'s", '820', '430', '0707', '528', '188', '181', '4EVA', '989', '432', '1412', '204', '404', '1110', '0406', '304', '428', '5000', '0405', '825', '0809', '3s', '??', '628', '0204', '281', '2212', '3d', '0205', '197', '0202', '1115', '1954', '726', '728', '237', '0505', '757', '1116', '302', '140', '954', '559', '601', '747', '1516', '405', '730', '0305', '243', '146', '154', '509', '729', '627', '0306', '1415', '"', '350', '629', '1324', '696', '0507', '0711', '0214', '253', '269', '407', '0407', '0304', '306', '4U', '650', '2310', '171', '236', '2529', '630', '1905', '239', '2528', '450', '1s', '2527', '139', '025', '195', '&', '0303', '123*', '1118', '454', '0606', '2530', '0203', '876', '930', '178', "\\\\'", '2210', '0107', '0404', '027', '0408', '1907', '819', '0812', '409', '158', '908', '543', '401', '230', '0210', '1312', '0308', '307', '1love', '240', '0412', '1369', '1310', '1953', '826', '829', '2!', '165', '2105', '161', '606', '1234567890', '160', '148', '904', '0311', '830', '0307', '0103', '2211', '2014', '193', '.12', '0110', '308', '827', '1307', '0420', '0206', '1119', '0207', '@@', '0911', '0212', '149', '990', '0105', '2123', '.2', '255', '309', '(L)', '241', '3333', '801', '0912', '185', '1432', '0104', '6666', '0987', '175', '163', '502', '1950', '671', '0106', '0208', '765', '251', '337', '1908', '170', '445', '166', '028', '173', '257', '3y', '031', '2107', '1888', '2524', '123123', '196', '&me', '1402', '0608', '2311', '1331', '!@#', '1903', '1305', '0312', '1235', '1@', '956', '174', '2312', '0509', '275', '2523', '0508', '172', '0512', '1311', '1306', '1952', '1200', '90210', '1512', '1408', '1233', '026', '1411', '2727', '1920', '978', '184', '5683', '1690', '0810', '2510', '244', '0709', '565', '2410', '667', '238', '090', '0108', '164', '12345678', '55555', '4u2', '0411', '1912', '#7', '3n', '2412', '2205', '1812', '1904', '0811', '2103', '402', '0612', '0309', '973', '0907', '336', '14344', '1410', '123321', '1906', '1323', '289', '903', '\\\\', '0211', '0209', '1405', '2106', '2828', '0712', '803', '2580', '2626', '267', '0321', '1308', '1718', '>', '2207', '1304', '262', '4ev', '077', '179', '343', '2531', '2110', '3030', '1508', '1407', '12!', '2203', '162', '0x', '585', '0906', '176', '1821', '1606', '2303']
 BASIC_PREFIXS = ['1', '2', '4', '123', '3', '*', '12', '7', '5', '8', '6', '13', '11', '143', '9', '19', '@', '(', '10', '14', '0', '22', '23', '21', '$', '!', '#1', '20', '15', '24', '18', '17', '16', '1234', '69', '.', '25', '01', '27', 'i', '07', '26', '**', '28', '06', '29', '08', '30', '~', '00', '05', '123456', '88', '99', '12345', '02', '03', '100', '04', '31', '09', '[', '666', '33', '77', '#', 'sk8', '50', 'ms.', 'mr.', '123456789', '1994', 'i<3', '100%', 'mz.', '<', '89', 'l0', '44', '1992', '1993', '420', '32', '1995', '55', '2007', '101', '34', '321', '1991', '007', '92', ',', 'm1', '2006', '87', '95', '1989', '"', '<3', '1996', '1990', '98', 'no1', '93', '1987', '777', 'm0', '94', 'c0', '90', '66', '2008', '45', ';', 'r0', '91', '96', 'il0', '86', '1988', 'my1', 'ih8', '78', '97', 'my', 'h0', '..', '+', '!!', '111', 'p0', 'l1', 'j0', '56', 'b1', 'm3', '85', '1986', '1985', '52', 'd3', '35', '42', 'k1', '/', 's3', '2005', '84', '40', '79', '`', '82', '619', '***', 's0', '54', 'p1', 'b3', '76', 'te', '36', '74', '67', 'a1', 'g0', 'hi5', '=', '83', 'd0', '1997', '911', '333', '1984', 'b0', '49', 's1', '456', 'd1', '68', '555', 'm@', '80', '72', '43', 'j.', '?', '$$', '...', 'j3', '789', 'h3', '999', '187', '37', '1983', '75', '57', 'n0', '41']
@@ -151,7 +154,19 @@ def generate_usernames(person_name):
     ]
 
     return combinations
-    
+
+
+def normalize_list(input_):
+    file_or_not = detec_if_file_or_not(input_)
+    if file_or_not == True:
+        with open(file_name, 'r', encoding='utf-8') as file_:
+            words_list = [line.strip() for line in file_ if line.strip()]
+            return words_list
+    else:
+        raw_list = input_.strip().split(',')
+        words_list = [word.strip() for word in raw_list if word.strip()]
+        return words_list
+
 def process_file_user(file_name, output_file_name):
     global OUTPUT_FILE_BULEAN
     with open(file_name, 'r', encoding='utf-8') as file_:
@@ -188,59 +203,23 @@ def process_input_user(input_, output_file_name):
         for name in people_list:
             final_list += generate_usernames(name) 
         save_list_to_file(final_list, output_file_name)
-
-def process_file_passwd(file_name, output_file_name):
+  
+def process_passwd(words_list, output_file_name):
     global OUTPUT_FILE_BULEAN
     global FULL_MODE
     global LIGHT_MODE
-    with open(file_name, 'r', encoding='utf-8') as file_:
-        words_list = [line.strip() for line in file_ if line.strip()]
     
     # Checking for massive mode:
-    if len(words_list) >= 10 and FULL_MODE == True:
+    if len(words_list) >= 5 and FULL_MODE == True:
         massive_mode(words_list, output_file_name)
         sys.exit(0)
-    if len(words_list) >= 100 and LIGHT_MODE == False:
-        massive_mode(words_list, output_file_name)
-        sys.exit(0)       
-    if len(words_list) >= 1000:
-        massive_mode(words_list, output_file_name)
-        sys.exit(0)      
-
-            
-    if not OUTPUT_FILE_BULEAN:
-        for line in words_list:
-            combinations = generate_password_list(line)
-            for element in combinations:
-                print(element)
-    else:
-        complet_list = []
-        for line in words_list:
-            combinations = generate_password_list(line)
-            complet_list.extend(combinations)
-        save_list_to_file(complet_list, output_file_name)
-                
-            
-
-
-def process_input_passwd(input_, output_file_name):
-    global OUTPUT_FILE_BULEAN
-    global FULL_MODE
-    global LIGHT_MODE
-    raw_list = input_.strip().split(',')
-    words_list = [word.strip() for word in raw_list if word.strip()]
-    
-    # Checking for massive mode:
-    if len(words_list) >= 10 and FULL_MODE == True:
-        massive_mode(words_list, output_file_name)
-        sys.exit(0)
-    if len(words_list) >= 100 and LIGHT_MODE == False:
+    if len(words_list) >= 50 and LIGHT_MODE == False:
         massive_mode(words_list, output_file_name)
         sys.exit(0) 
-    if len(words_list) >= 1000:
+    if len(words_list) >= 500:
         massive_mode(words_list, output_file_name)
         sys.exit(0)
-    
+
     final_list = []
     if OUTPUT_FILE_BULEAN == False:
         for word in words_list:
@@ -250,7 +229,52 @@ def process_input_passwd(input_, output_file_name):
     else:
         for word in words_list:
             final_list += generate_password_list(word)
-        save_list_to_file(final_list, output_file_name)      
+        save_list_to_file(final_list, output_file_name)
+        
+def find_neighbours(model,word,number):
+    pre_words = model.get_nearest_neighbors(word, k=number)
+    semi_words = [text for _, text in pre_words]
+    almost_words = [text for text in semi_words if '.' not in text]
+    def valid(text):
+        return all(not c.isupper() for c in text[1:])
+    words = [text for text in almost_words if valid(text)]
+    return words
+
+def ml_process_pwd(list_, ml_model, number_neighbours):
+    system_os = system_detection()
+    if system_os == 'linux':
+        try:
+            import fasttext
+            try:
+                model = fasttext.load_model(ml_model)
+                verbose_print("[+] Fasttext module loaded successfully.")
+                if hasattr(ml_model, 'predict') and callable(ml_model.predict):
+                    verbose_print("[+] Model loaded validated.")
+            except Exception:
+                print("[-] Sorry but doesn't looks like your input file is a valid ML model.")
+                print("  You can get this Machine Learning traind models from this website:")
+                print("  https://fasttext.cc/docs/en/pretrained-vectors.html")
+                print("  You need the .bin file for the lenguage you want.")
+                sys.exit(1)
+
+            verbose_print("[+] Looking for the " + str(number_neighbours) + " nearest neighbours for each word.")
+            for word in list_:
+                print("[+] Processing -> "+str(word))
+                neighbors = find_neighbours(model, word, number_neighbours)
+                words_list = []
+                for neighbor in neighbors:
+                    words_list.append(neighbor)
+            return words_list
+
+        except ImportError:
+            print("[-] Unable to load fasttext module. Plese install it:")
+            print("  $ git clone https://github.com/facebookresearch/fastText.git\n  $ cd fastText\n  $ sudo pip install .")
+            sys.exit(1)
+
+    if system_os == 'windows':
+        print('[-] Sorry, but Windows is not supported for machine learning module')
+        sys.exit(1)
+
         
 def massive_mode(list_, output_file_name):
     global LIGHT_MODE
@@ -421,6 +445,7 @@ def main():
     global BASIC_PREFIXS
     global NUMERIC_PATTERNS
     global SYMBOLIC_PATTERNS
+    global NEIGHBORS_AMMOUNT
     
     if len(sys.argv) == 1:
         print_banner()
@@ -439,6 +464,7 @@ def main():
     parser.add_argument('-d','--dictionary', metavar='file_name', help='Extract patterns from your an specific dictionary')
     parser.add_argument('-o', '--output', metavar='file_name', help='Dictionary will be stored in this file.')
     parser.add_argument('-ml', '--machine-learning-model', metavar='file_name', help='Use a trained machine learning model to include neighbors of your original words')
+    parser.add_argument('-n', '--neighbours-number', metavar='integer', help='Ammount of neighbors for each word (20 by Default).')
 
     args = parser.parse_args()
     
@@ -449,17 +475,14 @@ def main():
         VERBOSE = False
     if args.light:
         LIGHT_MODE = True
-        verbose_print("Light mode, for small list.")
+        verbose_print("[+] Light mode enabled")
     if args.full:
         FULL_MODE = True
-        verbose_print("Full mode. Warning, the output could be very heavy.")
+        verbose_print("[+] Full mode enabled. Warning, the output could be very heavy.")
     if args.output:
         OUTPUT_FILE_BULEAN = True
         output_file_name = args.output
         verbose_print("Dictionary will be stored in this file -> "+str(output_file_name))
-    if args.machine_learning_model:
-        print('[-] Sorry, MACHINE LEARNING section is under construction yet!, Exiting...')
-        sys.exit(1)
         
     if args.dictionary is not None:
         if is_a_valid_file(args.dictionary):
@@ -489,21 +512,24 @@ def main():
             sys.exit(0)
 
     elif args.password is not None:
-        if args.machine_learning_model is not None:
-            #file_or_not = detec_if_file_or_not(args.password)
-            #ml_list = machine_learning_cracking(args.machine_learning_model)
-            sys.exit(0)
         if args.password.strip() == "":
             print("Error: This argument can not be empty", file=sys.stderr)
             sys.exit(1)
+        input_list = normalize_list(args.password)
+        
+        if args.machine_learning_model is not None:                  # Section under development
+            if detec_if_file_or_not(args.machine_learning_model) == False:
+                print("[-] Your -ml <input> is not even a file...  ¬¬ , set a file here")
+                sys.exit(1)
+            if args.neighbours_number is not None:
+                NEIGHBORS_AMMOUNT = int(args.neighbours_number)
+            ml_list = ml_process_pwd(input_list, args.machine_learning_model, NEIGHBORS_AMMOUNT)
+            process_passwd(ml_list, output_file_name)
+            sys.exit(0)
+
         verbose_print("[+] PASSWORD mode selected.")
-        file_or_not = detec_if_file_or_not(args.password)
-        if file_or_not == True:
-            process_file_passwd(args.password, output_file_name)
-            sys.exit(0)
-        else:
-            process_input_passwd(args.password, output_file_name)
-            sys.exit(0)
+        process_passwd(input_list, output_file_name)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
